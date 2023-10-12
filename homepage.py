438b645371ad9a04054906e3d0152b497df539fd
Sam Sum <samsum@corporate.com>
2023-10-12 11:05:00 +0100

    Fixed syntax errors

--- homepage.py
+++ homepage.py
@@ -1,8 +1,8 @@
-import tkinter as tk
+from tkinter import *
 
-window = tk()
+window = Tk()
 window.title("Girl Boss Clickers")
-window.geometry("1000*1000")
+window.geometry("1000x1000")
 bg = PhotoImage(file = "background.png")
 
 bgLabel = Label(window, image = bg)
