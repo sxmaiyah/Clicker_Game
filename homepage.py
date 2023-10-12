fb376144cc5a620ffb070fe742caca9b5ec6d6c8
Issie <isobel.mcgarry@gmail.com>
2023-10-12 11:01:50 +0100

    Homepage not working :(

--- homepage.py
+++ homepage.py
@@ -1,6 +1,11 @@
-from tkinter import *;
-from tkinter.ttk import;
+import tkinter as tk
 
 window = tk()
 window.title("Girl Boss Clickers")
-window.geometry(1000*1000)
+window.geometry("1000*1000")
+bg = PhotoImage(file = "background.png")
+
+bgLabel = Label(window, image = bg)
+bgLabel.place(x=0, y=0)
+
+window.mainloop()
