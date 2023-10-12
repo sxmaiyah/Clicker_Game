02154160d55e0efe1dc5b5b756f1da88e7eb9058
Issie <isobel.mcgarry@gmail.com>
2023-10-12 11:21:16 +0100

    no bg

--- homepage.py
+++ homepage.py
@@ -4,9 +4,9 @@ window = Tk()
 window.title("Girl Boss Clickers")
 window.geometry("1000x1000")
 window.resizable(width=False, height=False)
-bg = PhotoImage(file = "background.png")
+#bg = PhotoImage(file = "background.png")
 
-bgLabel = Label(window, image = bg)
-bgLabel.place(x=0, y=0)
+#bgLabel = Label(window, image = bg)
+#bgLabel.place(x=0, y=0)
 
 window.mainloop()
