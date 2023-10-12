2a72ad7cbd02fab630f429613c93a8c726b5843b
ammarah <u1964819@unimail.hud.ac.uk>
2023-10-12 11:34:19 +0100

    Buttons made

--- homepage.py
+++ homepage.py
@@ -9,4 +9,32 @@ window.resizable(width=False, height=False)
 #bgLabel = Label(window, image = bg)
 #bgLabel.place(x=0, y=0)
 
+def gamePage():
+    window.destroy()
+    #import girlbossclickers
+
+Button(
+    window,
+    text="Issie",
+    command=gamePage
+    ).pack(fill=X, expand=TRUE, side=LEFT)
+
+Button(
+    window,
+    text="Ammarah",
+    command=gamePage
+    ).pack(fill=X, expand=TRUE, side=LEFT)
+
+Button(
+    window,
+    text="Sumaiyah",
+    command=gamePage
+    ).pack(fill=X, expand=TRUE, side=LEFT)
+
+Button(
+    window,
+    text="Sam",
+    command=gamePage
+    ).pack(fill=X, expand=TRUE, side=LEFT)
+
 window.mainloop()
