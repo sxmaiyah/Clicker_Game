a45e89e74cc1db650bb8d46674e326685b45a981
Issie <isobel.mcgarry@gmail.com>
2023-10-12 11:41:52 +0100

    Button images

--- homepage.py
+++ homepage.py
@@ -3,7 +3,7 @@ from tkinter import *
 window = Tk()
 window.title("Girl Boss Clickers")
 window.geometry("1000x1000")
-window.resizable(width=False, height=False)
+#window.resizable(width=False, height=False)
 #bg = PhotoImage(file = "background.png")
 
 #bgLabel = Label(window, image = bg)
@@ -13,27 +13,43 @@ def gamePage():
     window.destroy()
     #import girlbossclickers
 
+issiephoto = PhotoImage(file = "izzyface.png")
+ammarahphoto = PhotoImage(file = "ammarahface.png")
+samphoto = PhotoImage(file = "samface.png")
+sumphoto = PhotoImage(file = "sumface.png")
+chrisphoto = PhotoImage(file = "chrisface.png")
+
 Button(
     window,
-    text="Issie",
+    text="Issie", image = issiephoto,
     command=gamePage
     ).pack(fill=X, expand=TRUE, side=LEFT)
 
 Button(
     window,
-    text="Ammarah",
+    text="Ammarah", 
+    image = ammarahphoto,
     command=gamePage
     ).pack(fill=X, expand=TRUE, side=LEFT)
 
 Button(
     window,
-    text="Sumaiyah",
+    text="Sumaiyah", 
+    image = sumphoto,
     command=gamePage
     ).pack(fill=X, expand=TRUE, side=LEFT)
 
 Button(
     window,
-    text="Sam",
+    text="Sam", 
+    image = samphoto,
+    command=gamePage
+    ).pack(fill=X, expand=TRUE, side=LEFT)
+
+Button(
+    window,
+    text="Chris", 
+    image = chrisphoto,
     command=gamePage
     ).pack(fill=X, expand=TRUE, side=LEFT)
 
